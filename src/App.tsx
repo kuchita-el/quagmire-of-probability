11da6ad64b098d811e07bc97f6ae7e91bbd6023e
daisuke kurata <d.kruata.38@gmail.com>
2021-11-22 20:19:10 +0900

    90%ラインの期待値回数を計算できる機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./organisms/Header";
+import {Container} from "@mui/material";
+import ProbabilityForm from "./organisms/ProbabilityForm";
 
 function App() {
-  return (
-      <div>
-        <Header title={"マビ率は沼"}/>
-      </div>
-  );
+    return (
+        <div>
+            <Header title={"マビ率は沼"}/>
+            <Container>
+                <ProbabilityForm/>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
